9cbc5b80705e5c8f4a01d5f02d2d263dbe2e4f99
Gustavo Luna <marcacacahuate@outlook.com>
2024-04-28 21:57:43 -0600

    feat: Add package django-tailwind

--- projectmyss/urls.py
+++ projectmyss/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import render
+
+
+def test_home(request):
+    return render(request, "base.html")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', test_home, name="home_test"),
+    # tailwind routes
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
