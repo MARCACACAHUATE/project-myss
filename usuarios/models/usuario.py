9ae6394c85fe61cf7768625b23e889714d3bd609
Gustavo Luna <marcacacahuate@outlook.com>
2024-05-07 00:33:36 -0600

    fix: Removi el constraint unique del campo email en el modelo Usuario y agregue el campo fecha_creacion en el modelo Propuestas

--- usuarios/models/usuario.py
+++ usuarios/models/usuario.py
@@ -8,7 +8,7 @@ class Usuario(AbstractBaseUser, PermissionsMixin):
     numero_empleado = models.CharField(max_length=8, unique=True)
     nombre = models.CharField(max_length=100)
     departamento = models.CharField(max_length=50)
-    correo = models.EmailField(unique=True)
+    correo = models.EmailField()
     password = models.TextField()
     role_id = models.ForeignKey("Role", on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
