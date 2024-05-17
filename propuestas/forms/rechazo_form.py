585c85bc2ed75e9fa988b52734158ccf0e6420e6
juancatap <juantapmtz02@gmail.com>
2024-05-17 16:44:08 -0600

    Reapply "Merge branch 'main' of https://github.com/MARCACACAHUATE/project-myss"
    This reverts commit 83ef4a189d7b96f94b5c28bfe0299738eb8234c3.

--- propuestas/forms/rechazo_form.py
+++ propuestas/forms/rechazo_form.py
@@ -1,15 +1,17 @@
 from django import forms
+from usuarios.models import Motivo
 
 
 class RechazarPropuestaForm(forms.Form):
-    opciones = (
-        ("Opcion 1", "Opcion 1"),
-        ("Opcion 2", "Opcion 2"),
-        ("Opcion 3", "Opcion 3"),
-        ("Opcion 4", "Opcion 4"),
-    )
+    motivos_rechazo = Motivo.objects.all()
+
+    opciones = [(motivo.Titulo, motivo.Descripcion)
+                for motivo in motivos_rechazo]
 
     descripcion = forms.CharField(
         widget=forms.Textarea(attrs={"rows": 5, }), required=False)
     motivo_rechazo = forms.ChoiceField(
         choices=opciones, label="Seleccionar el motivo del rechazo")
+
+    def convert_into_tuple(self, lista):
+        return tuple((lista.Motivo, lista.Motivo) for motivo in lista)
--- propuestas/views/propuestas_detalles.py
+++ propuestas/views/propuestas_detalles.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from propuestas.models import Propuesta
 from propuestas.utils import Status
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 class PropuestasDetallesView(LoginRequiredMixin, View):
@@ -19,9 +21,10 @@ class PropuestasDetallesView(LoginRequiredMixin, View):
 
     def post(self, request, *args, **kwargs):
         id = self.kwargs['propuesta_id']
-
         propuesta = Propuesta.objects.get(id=id)
         propuesta.status = Status.ACEPTADA
+        send_mail(subject=settings.EMAIL_HOST_ASUNTO, message="Enhorabuena su propuesta ha sido acepatada",
+                  from_email=settings.EMAIL_HOST_USER, recipient_list=[propuesta.correo_personal,
+                                                                       propuesta.correo_institucional])
         propuesta.save()
-
         return render(request, self.template_name, {"propuesta_data": propuesta})
