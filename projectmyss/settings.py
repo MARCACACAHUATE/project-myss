6e9ac59c90e99a31aae9811c9d0a926b21bce15e
IAL8 <irvingatln@gmail.com>
2024-05-24 03:01:32 -0600

    Haber que 2

--- projectmyss/settings.py
+++ projectmyss/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # packages
-    'tailwind',
+    'django_tailwind',
     'theme',
     'django_browser_reload',
     # custom apps
@@ -152,8 +152,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
