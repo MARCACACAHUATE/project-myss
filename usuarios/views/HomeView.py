94460fc8c3d74c0bb4eed951faea3e44781c03f0
IAL8 <irvingatln@gmail.com>
2024-05-24 14:34:14 -0600

    Haber que 58900

--- usuarios/views/HomeView.py
+++ usuarios/views/HomeView.py
@@ -1,8 +1,11 @@
 from django.views import View
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 
 
+@method_decorator(cache_page(60 * 15), name='dispatch')
 class HomeView(LoginRequiredMixin, View):
     login_url = "/login/"
     template_name = "Home.html"
